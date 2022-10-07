d58427338108bd383fa32a7cee522ab35aeb6053
Konstadinos Raidos <kostasraid96@gmail.com>
2022-10-07 20:51:54 +0300

    New-Schema

--- Models/Product.model.js
+++ Models/Product.model.js
@@ -1,17 +1,45 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
+
+//Sheama της βάσης στο ATLAS.
 var ProductScheama = new Schema({
-  name: {
+  Name: {
     type: String,
     required: true
   },
-  price: {
+  Description: {
+    type: String
+  },
+  Price: {
     type: String,
     required: true
+  },
+  Sale: {
+    type: Boolean,
+    default: false
+  },
+  Photo: {
+    type: String
+  },
+  ZipCode: {
+    type: Number
+  },
+  Region: {
+    type: String
+  },
+  Roof: {
+    type: Number
+  },
+  Area: {
+    type: Number
+  },
+  Bedrooms: {
+    type: Number
   }
 });
 
 
+//Table=Collections ==> products
 const Product = mongoose.model('product', ProductScheama);
 module.exports = Product;
--- Routes/Product.route.js
+++ Routes/Product.route.js
@@ -1,66 +1,106 @@
 const express = require("express");
 const router = express.Router();
+const createError = require("http-errors");
+const mongoose = require("mongoose");
 
 const Product = require("../Models/Product.model");
 
-//http://127.0.0.1:3000Products
+//http://127.0.0.1:3000/Products
+//Getting a list of all products
 router.get("/", async (req, res, next) => {
   try {
-    const results = await Product.find({}, { name: 1, price: 1 });
+    const results = await Product.find({}, { 
+      Name: 1, 
+      Price: 1, 
+      Description: 1,
+      Sale: 1,
+      Photo: 1,
+      ZipCode: 1,
+      Region: 1,
+      Roof: 1,
+      Area: 1,
+      Bedrooms: 1 });
     res.send(results);
   } catch (error) {
     console.log(error.message);
   }
-  //res.send("Getting a list of all products..");
 });
 
+//POST a New Product in DB
 router.post("/", async (req, res, next) => {
   try {
     const product = new Product(req.body);
     const result = await product.save();
     res.send(result);
   } catch (error) {
-    console.log(error.message);
+    if(error.name === 'validationError'){
+      console.log(error.message);
+      next(createError(422, error.message))
+      return
+    }
+    next(error)
   }
 });
 
+//Return in JSON all the results from a specific Product με βάση το ID στην DB
 //http://127.0.0.1:3000Products/ID
 router.get("/:id", async (req, res, next) => {
   const id = req.params.id;
   try {
-    //const product = await Product.findById(id)
+    //findOne => {Query} {Attributes}
     const product = await Product.findOne({ _id: id }, { __v: 0 });
+
+    if (!product) {
+      throw createError(404, "Product does not exist.");
+    }
     res.send(product);
   } catch (error) {
-    console.log(error.message);
+    if (error instanceof mongoose.CastError) {
+      next(createError(400, "Invalid Product ID"));
+      return;
+    }
+    //Call the error hundler in app.js for 404
+    next(error);
   }
 });
 
-router.patch("/:id", async(req, res, next) => {
-  //res.send("Update a product..");
-  console.log("nnnnnnnnnnnnnnnnnnnnnn");
+//Για update ένα Product με βάση το ID
+router.patch("/:id", async (req, res, next) => {
   try {
-    
     const id = req.params.id;
-    const updates = req.body
-    const options = {new: true}
-    
-    const result = await Product.findByIdAndUpdate(id, updates, options)
-    res.send(result)
-  
+    const updates = req.body;
+    const options = { new: true };
+
+    const result = await Product.findByIdAndUpdate(id, updates, options);
+    if(!result){
+      throw createError(404,'Product does not exist')
+    }
+    res.send(result);
   } catch (error) {
     console.log("Error ............ " + error.message);
+    if (error instanceof mongoose.CastError) {
+      return next(createError(400, "Invalid Product ID"));
+    }
+    next(error)
   }
 });
 
+//delete ένα PRODUCT με βάση το ID
 router.delete("/:id", async (req, res, next) => {
   const id = req.params.id;
   try {
-    //const product = await Product.findById(id)
     const result = await Product.findOneAndDelete({ _id: id });
+    if (!result) {
+      throw createError(404, "Product does not exist.");
+    }
     res.send(result);
   } catch (error) {
-    console.log(error.message);
+    if (error instanceof mongoose.CastError) {
+      next(createError(400, "Invalid Product ID"));
+      return;
+    }
+    //Call the error hundler in app.js for 404
+    next(error);
   }
 });
 
