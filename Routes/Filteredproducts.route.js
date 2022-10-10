86019940a03e5d1d17b3201084dc72b561b7e8de
Theodore Dimopoulos <tdimopoulosprof@gmail.com>
2022-10-10 17:01:36 +0300

    filters_edit2

--- Routes/Filteredproducts.route.js
+++ Routes/Filteredproducts.route.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const createHttpError = require("http-errors");
 const router = express.Router();
 
 const Product = require("../Models/Product.model");
@@ -23,11 +24,13 @@ router.get('/polisi', async (req, res) => {
 
 });
 
+//search db with multiple parameters
 router.get("/:query", async (req, res) => {
 
     const filter = await Product.find(req.query);
 
     if(!filter) {
+        //throw createHttpError(404, "Product does not exist.")
         res.status(500).json({success: false})
     }
     res.send({filter});
