e58b68a07101227ae8f96d6c24c928fbe188433a
Theodore Dimopoulos <tdimopoulosprof@gmail.com>
2022-10-10 17:38:18 +0300

    b

--- Routes/Filteredproducts.route.js
+++ Routes/Filteredproducts.route.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const createHttpError = require("http-errors");
 const router = express.Router();
 
 const Product = require("../Models/Product.model");
@@ -30,7 +29,6 @@ router.get("/:query", async (req, res) => {
     const filter = await Product.find(req.query);
 
     if(!filter) {
-        //throw createHttpError(404, "Product does not exist.")
         res.status(500).json({success: false})
     }
     res.send({filter});
