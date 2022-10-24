c620f018f796a7c82c51f02aaa3033e2bcd5c771
Theodore Dimopoulos <tdimopoulosprof@gmail.com>
2022-10-24 11:10:22 +0300

    chatbot.v2

--- api/routes/chatbot.js
+++ api/routes/chatbot.js
@@ -1,24 +1,110 @@
-//const bodyParser = require("body-parser");
 const express = require("express");
 const router = express.Router();
-const dialogflow = require("@google-cloud/dialogflow");
+const cleanDeep = require('clean-deep');
+const mongoose = require("mongoose");
+const Product = require("../models/product");
+const { WebhookClient, Payload } = require("dialogflow-fulfillment");
+const product = require("../models/product");
 
-const { WebhookClient } = require('dialogflow-fulfillment');
+/*const dotenv = require("dotenv");
+dotenv.config();*/
 
-router.post("/", (request, response) =>{
-    const _agent = new WebhookClient({request: request, response: response});
+router.post("/", async (req, res) => {
+  const agent = new WebhookClient({ request: req, response: res });
 
 
-    function respond(agent) {
-        return agent.add("ok thats great you finished from back end");
-    };
+  const reqData = req.body.queryResult.outputContexts[0].parameters;
 
-    let intents = new Map();
+function replacer (key, value) {
+  if (value === "" || value===null || value===undefined || key.includes("original") || value==="null"){
+    delete key
+  }else{
+    return value
+  }
+}
 
-    intents.set("finalize-yes", respond);
-    _agent.handleRequest(intents);
-    
+console.log("fffffffff:  " + JSON.stringify(reqData, replacer))
+
+const dataString = JSON.stringify(reqData, replacer)
+var dataParsed = JSON.parse(dataString)
+
+  //console.log("dd:" + JSON.stringify(reqData))
+  const data = {
+    Sale: false,
+    Region: reqData.city,
+    Price: {
+      "$lte": dataParsed.maxPrice,
+      "$gte": dataParsed.minPrice,
+    },
+    Roof: dataParsed.floor,
+    Area: {
+      "$lte": dataParsed.sqMax,
+      "$gte": dataParsed.sqMin,
+    },
+  };
+  
+
+  var finalData  = {}
+  finalData = cleanDeep(data) 
+  console.log("finaldata: " + JSON.stringify(cleanDeep(data)))
+
+
+
+  //console.log("kkk:  " +JSON.parse(data))
+
+  
+  var url = "http://localhost:3000/Search?page=1";
+  for (let i in finalData) {
+    if (typeof finalData[i] == "object" && finalData[i]!=NaN && finalData[i]!={} && Object.keys(finalData[i]).length!==0) {
+      for (let j in finalData[i]) {
+        if (finalData[i][j] !== undefined) {
+          //if (data[i][j] !== "") {
+            //console.log("ddddddddd: " + finalData[i][j])
+            url += "&" + i + j + "=" + finalData[i][j];
+          //}
+        }
+      }
+      
+    } else {
+      if (finalData[i] != undefined) {
+        //if (data[i] !== "") {
+          url += "&" + i + "=" + finalData[i];
+          
+        //}
+      }
+    }
+  }
+
+  
+  //console.log("ggggg:  " + Object.values(data))
+  //const queryParsed = JSON.parse(query)
+  //console.log("qqqqq: " + JSON.stringify(queryParsed, replacer))
+  //console.log("data:   " + JSON.stringify(data, replacer));
+  console.log("url:   " + url);
+
+
+try {
+ var houses = await product.find({"Roof": 4}).select('_id').limit(3).sort('-Price')  
+} catch (error) {
+  console.log(error.message)
+}
+
+console.log (JSON.stringify(houses))
+
+  function findHome(agent) {
+    agent.add("http://localhost:3000/Products/"+houses[0]._id)
+    agent.add("http://localhost:3000/Products/"+houses[1]._id)
+    agent.add("http://localhost:3000/Products/"+houses[2]._id)
+    agent.add("For more...")
+    agent.add(url)
+  }
+
+
+  var intentMap = new Map();
+  //intentMap.set("Default Welcome Intent", Default);
+  intentMap.set("finalize-yes", findHome);
+  intentMap.set("optional filters", findHome);
+  agent.handleRequest(intentMap);
 });
 
 module.exports = router;
-
